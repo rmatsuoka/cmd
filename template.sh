effcd96ca5ff8b63986f181baefad46d08efec85
Ryuichi Matsuoka <r.matsuoka@protonmail.com>
2022-04-18 17:36:35 +0900

    template.sh: renewal

--- template.sh
+++ template.sh
@@ -5,39 +5,24 @@ export IFS='
 syspath=$(command -p getconf PATH 2>/dev/null)
 export PATH="${syspath:-/bin:/usr/bin}${PATH:+:$PATH}"
 progname=$(basename "$0")
-synposis="$progname [-h]"
 
 warn(){
 	printf '%s: %s\n' "$progname" "$*" >&2
 }
 
 fatal(){
-	[ -n "$1" ] && warn "$1"
-	printf 'usage: %s\n' "$synposis" >&2
+	warn "$1"
 	exit ${2-"1"}
 }
 
 usage(){
-	cat >&2 <<EOF
-NAME
-	$progname -- a template for shellscript
-
-SYNOPSIS
-	$synposis
-
-OPTIONS
-	-h	show this
-
-EXAMPLE
-	\$ $progname
-EOF
+	printf 'usage: %s\n' "$progname" >&2
 	exit 1
 }
 
 while getopts h flag; do
 	case "$flag" in
-	h)	usage;;
-	?)	fatal;;
+	h|?)	usage;;
 	esac
 done
 shift $((OPTIND - 1))
